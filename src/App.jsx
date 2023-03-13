d0f9153508279c16581a9b99e0fbda0ba88c76cc
Leon San José Larsson <leonlarsson8@gmail.com>
2023-03-13 13:38:11 +0100

    hide stats if error

--- src/App.jsx
+++ src/App.jsx
@@ -107,42 +107,42 @@ const App = () => {
       <hr />
 
       <Space style={{ width: "100%" }} direction="vertical">
-        <Space style={{ width: "100%" }} direction="vertical">
-          <Input size="large" placeholder="Search for a user" onChange={e => filterUsers(e.target.value)} />
-          <Space>
-            <Button disabled={loading} onClick={fetchData}>Refresh data</Button>
-            <span>{usersToShow.length.toLocaleString("en-US")} {usersToShow.length === 1 ? "user" : "users"}</span>
-          </Space>
-          {error ?
-            <h1>Error</h1>
-            :
+        <Input size="large" placeholder="Search for a user" onChange={e => filterUsers(e.target.value)} />
+        <Space>
+          <Button disabled={loading} onClick={fetchData}>Refresh data</Button>
+          <span>{usersToShow.length.toLocaleString("en-US")} {usersToShow.length === 1 ? "user" : "users"}</span>
+        </Space>
+        {error ?
+          <h1>Error</h1>
+          :
+          <Space style={{ width: "100%" }} direction="vertical">
             <Table
               columns={columns}
               dataSource={usersToShow}
               scroll={{ x: true }}
               loading={loading}
             />
-          }
-        </Space>
 
-        <Collapse>
-          <Collapse.Panel header="Stats">
-            <Space style={{ width: "100%" }} direction="vertical">
+            <Collapse>
+              <Collapse.Panel header="Stats">
+                <Space style={{ width: "100%" }} direction="vertical">
 
-              <Divider style={{ marginBottom: 0 }} orientation="left">Out of the top {users.length.toLocaleString("en-US")} players...</Divider>
-              <span><Typography.Text code>{users.filter(user => user.fame >= 1000).length.toLocaleString("en-US")} ({(users.filter(user => user.fame > 1000).length / users.length).toLocaleString("en-US", { style: "percent" })})</Typography.Text> are in {getTrophy(1000)} Gold league</span>
-              <span><Typography.Text code>{users.filter(user => user.fame < 1000 && user.fame >= 500).length.toLocaleString("en-US")} ({(users.filter(user => user.fame < 1000 && user.fame >= 500).length / users.length).toLocaleString("en-US", { style: "percent" })})</Typography.Text> are in {getTrophy(500)} Silver league</span>
-              <span><Typography.Text code>{users.filter(user => user.fame < 500).length.toLocaleString("en-US")} ({(users.filter(user => user.fame < 500).length / users.length).toLocaleString("en-US", { style: "percent" })})</Typography.Text> are in {getTrophy(499)} Bronze league</span>
+                  <Divider style={{ marginBottom: 0 }} orientation="left">Out of the top {users.length.toLocaleString("en-US")} players...</Divider>
+                  <span><Typography.Text code>{users.filter(user => user.fame >= 1000).length.toLocaleString("en-US")} ({(users.filter(user => user.fame > 1000).length / users.length).toLocaleString("en-US", { style: "percent" })})</Typography.Text> are in {getTrophy(1000)} Gold league</span>
+                  <span><Typography.Text code>{users.filter(user => user.fame < 1000 && user.fame >= 500).length.toLocaleString("en-US")} ({(users.filter(user => user.fame < 1000 && user.fame >= 500).length / users.length).toLocaleString("en-US", { style: "percent" })})</Typography.Text> are in {getTrophy(500)} Silver league</span>
+                  <span><Typography.Text code>{users.filter(user => user.fame < 500).length.toLocaleString("en-US")} ({(users.filter(user => user.fame < 500).length / users.length).toLocaleString("en-US", { style: "percent" })})</Typography.Text> are in {getTrophy(499)} Bronze league</span>
 
-              <Divider style={{ marginBottom: 0 }} orientation="left">Averages</Divider>
-              <span>Average XP: <Typography.Text code>{(users.map(user => user.xp).reduce((a, b) => a + b, 0) / users.length).toLocaleString("en-US", { maximumFractionDigits: 0 })}</Typography.Text></span>
-              <span>Average Level: <Typography.Text code>{(users.map(user => user.level).reduce((a, b) => a + b, 0) / users.length).toLocaleString("en-US", { maximumFractionDigits: 0 })}</Typography.Text></span>
-              <span>Average Cashouts: <Typography.Text code>{(users.map(user => user.cashouts).reduce((a, b) => a + b, 0) / users.length).toLocaleString("en-US", { style: "currency", currency: "USD", maximumFractionDigits: 0 })}</Typography.Text></span>
-              <span>Average Fame: <Typography.Text code>{(users.map(user => user.fame).reduce((a, b) => a + b, 0) / users.length).toLocaleString("en-US", { maximumFractionDigits: 0 })}</Typography.Text></span>
+                  <Divider style={{ marginBottom: 0 }} orientation="left">Averages</Divider>
+                  <span>Average XP: <Typography.Text code>{(users.map(user => user.xp).reduce((a, b) => a + b, 0) / users.length).toLocaleString("en-US", { maximumFractionDigits: 0 })}</Typography.Text></span>
+                  <span>Average Level: <Typography.Text code>{(users.map(user => user.level).reduce((a, b) => a + b, 0) / users.length).toLocaleString("en-US", { maximumFractionDigits: 0 })}</Typography.Text></span>
+                  <span>Average Cashouts: <Typography.Text code>{(users.map(user => user.cashouts).reduce((a, b) => a + b, 0) / users.length).toLocaleString("en-US", { style: "currency", currency: "USD", maximumFractionDigits: 0 })}</Typography.Text></span>
+                  <span>Average Fame: <Typography.Text code>{(users.map(user => user.fame).reduce((a, b) => a + b, 0) / users.length).toLocaleString("en-US", { maximumFractionDigits: 0 })}</Typography.Text></span>
 
-            </Space>
-          </Collapse.Panel>
-        </Collapse>
+                </Space>
+              </Collapse.Panel>
+            </Collapse>
+          </Space>
+        }
       </Space>
     </div>
   );
