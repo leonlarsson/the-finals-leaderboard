875dbf1fe5dd3d33292f7fda1e72937b4b03f3d6
Leon San José Larsson <leonlarsson8@gmail.com>
2023-03-19 22:30:35 +0100

    add link to GitHub

--- src/App.jsx
+++ src/App.jsx
@@ -127,7 +127,7 @@ const App = () => {
   return (
     <div className="container mb-12">
       <h1 className="text-4xl font-medium underline">THE FINALS - Unofficial Beta Leaderboard</h1>
-      <h5 className="text-xl">Top {users.length.toLocaleString("en-US")} players from the current playtest. You can find the official leaderboard <a href="https://www.reachthefinals.com/leaderboard-beta" target="_blank" className="link">here</a>. Created by <a href="https://twitter.com/mozzyfx" target="_blank" className="link">me</a>.</h5>
+      <h5 className="text-xl">Top {users.length.toLocaleString("en-US")} players from the current playtest. You can find the official leaderboard <a href="https://www.reachthefinals.com/leaderboard-beta" target="_blank" className="link">here</a>. Created by <a href="https://twitter.com/mozzyfx" target="_blank" className="link">me</a>. Source <a href="https://github.com/leonlarsson/the-finals-leaderboard" target="_blank" className="link">here</a>.</h5>
       <hr />
 
       <Space className="w-full" direction="vertical">
