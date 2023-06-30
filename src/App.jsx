4c4f7612824d6e763d25c1c50ee800e7d2c51230
Leon San José Larsson <leonlarsson8@gmail.com>
2023-06-30 14:32:29 +0200

    remove badge on CB2

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
       <h5 className="text-xl">Top players from the current playtest. You can find the official leaderboard <a href="https://www.reachthefinals.com/leaderboard-beta" target="_blank" className="link">here</a>. Created by <a href="https://twitter.com/mozzyfx" target="_blank" className="link">me</a>. Source <a href="https://github.com/leonlarsson/the-finals-leaderboard" target="_blank" className="link">here</a>.</h5>
       <hr />
 
-      <Tabs defaultActiveKey="1" items={[{ key: "1", label: "Closed Beta 1", children: <Leaderboard betaVersion={"1"} /> }, { key: "2", label: <>Closed Beta 2 <Badge status="success" /></>, children: <Leaderboard betaVersion={"2"} /> }]} />
+      <Tabs defaultActiveKey="1" items={[{ key: "1", label: "Closed Beta 1", children: <Leaderboard betaVersion={"1"} /> }, { key: "2", label: "Closed Beta 2", children: <Leaderboard betaVersion={"2"} /> }]} />
     </div>
   );
 };
