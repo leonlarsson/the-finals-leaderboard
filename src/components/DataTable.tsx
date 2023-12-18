d16787db210e4993ce959fa82130376f4c01a8b0
Leon San José Larsson <leonlarsson8@gmail.com>
2023-12-18 16:40:33 +0100

    fix table flicker
    Thanks Dragory!

--- src/components/DataTable.tsx
+++ src/components/DataTable.tsx
@@ -113,7 +113,7 @@ export function DataTable<TData, TValue>({
       </div>
 
       <div className="rounded-md border">
-        <Table>
+        <Table className="table-fixed">
           <TableHeader>
             {table.getHeaderGroups().map(headerGroup => (
               <TableRow key={headerGroup.id}>
