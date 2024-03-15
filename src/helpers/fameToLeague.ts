6d98d420d413c8ffe5c6bf2e99cca7ca3e9c52a0
Leon San José Larsson <leonlarsson8@gmail.com>
2024-03-15 14:22:38 +0100

    fix league sorting
    Thanks Dragory!

--- src/helpers/fameToLeague.ts
+++ src/helpers/fameToLeague.ts
@@ -2,5 +2,5 @@ import { LEADERBOARD_VERSION, VERSION_LEAGUES } from "./leagues";
 
 export default (leaderboard: LEADERBOARD_VERSION, fame: number): string =>
   VERSION_LEAGUES[leaderboard]
-    .sort((a, b) => b.fame - a.fame)
+    .toSorted((a, b) => b.fame - a.fame)
     .find(x => fame >= x.fame)?.name ?? "Unknown";
