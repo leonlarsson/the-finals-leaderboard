226154e927669c6684af5cc5fc12788776fd6bd9
Leon San José Larsson <54620188+leonlarsson@users.noreply.github.com>
2024-10-01 00:25:10 +0200

    Stats: more s4s stats

--- src/components/Stats.tsx
+++ src/components/Stats.tsx
@@ -48,43 +48,64 @@ export default ({ leaderboardVersion, platform, users }: Props) => {
                   ).toLocaleString("en", { maximumFractionDigits: 0 })}
                 </span>
               </span>
+
+              <>
+                {sponsors.map(sponsor => (
+                  <span key={sponsor}>
+                    Average {sponsor} fans:{" "}
+                    <span className="rounded bg-neutral-200 px-1 dark:bg-neutral-800">
+                      {(
+                        users
+                          .filter(user => user.sponsor === sponsor)
+                          .map(user => user.fans!)
+                          .reduce((a, b) => a + b, 0) /
+                        users.filter(user => user.sponsor === sponsor).length
+                      ).toLocaleString("en", { maximumFractionDigits: 0 })}
+                    </span>
+                  </span>
+                ))}
+              </>
             </div>
 
             <hr className="my-2 border-black/30 dark:border-white/30" />
 
-            <span className="text-lg font-medium">
+            <div className="mb-3 text-lg font-medium">
               Out of the top {users.length.toLocaleString("en")}{" "}
               {leaderboard.name}
               {!leaderboard.disablePlatformSelection && (
                 <span> {platformName}</span>
               )}{" "}
               players...
-            </span>
+            </div>
 
-            <div className="grid grid-cols-2 gap-4 max-[850px]:grid-cols-1">
+            <div className="grid grid-cols-2 gap-4 text-center max-[850px]:grid-cols-1">
               <div className="flex flex-col items-center">
-                <DonutChart
-                  className="my-2"
-                  variant="pie"
-                  showLabel
-                  data={sponsors.map(sponsor => ({
-                    name: sponsor,
-                    value: users.filter(user => user.sponsor === sponsor)
-                      .length,
-                  }))}
-                  colors={["#0094df", "#f36b00", "#d31f3c"]}
-                  valueFormatter={v => `${v.toLocaleString("en")} players`}
-                  showAnimation
-                  animationDuration={500}
-                />
-                <Legend
-                  categories={sponsors}
-                  colors={["#0094df", "#f36b00", "#d31f3c"]}
-                />
-              </div>
-              <div>
-                <div className="mt-4">
-                  <div className="mb-1 font-medium">Players by sponsor</div>
+                <div className="mb-1 text-lg font-medium">
+                  Players by sponsor
+                </div>
+
+                <div className="space-y-3">
+                  <div className="flex flex-col items-center">
+                    <DonutChart
+                      className="my-2"
+                      variant="pie"
+                      showLabel
+                      data={sponsors.map(sponsor => ({
+                        name: sponsor,
+                        value: users.filter(user => user.sponsor === sponsor)
+                          .length,
+                      }))}
+                      colors={["#0094df", "#f36b00", "#d31f3c"]}
+                      valueFormatter={v => `${v.toLocaleString("en")} players`}
+                      showAnimation
+                      animationDuration={500}
+                    />
+                    <Legend
+                      categories={sponsors}
+                      colors={["#0094df", "#f36b00", "#d31f3c"]}
+                    />
+                  </div>
+
                   <div className="space-y-1">
                     {sponsors.map(sponsor => (
                       <div key={sponsor}>
@@ -111,9 +132,36 @@ export default ({ leaderboardVersion, platform, users }: Props) => {
                     ))}
                   </div>
                 </div>
+              </div>
 
-                <div className="mt-4">
-                  <div className="mb-1 font-medium">Fans by sponsor</div>
+              <div className="flex flex-col items-center">
+                <div className="mb-1 text-lg font-medium">Fans by sponsor</div>
+                <div className="space-y-3">
+                  <div className="flex flex-col items-center">
+                    <DonutChart
+                      className="my-2"
+                      variant="pie"
+                      showLabel
+                      data={sponsors.map(sponsor => ({
+                        name: sponsor,
+                        value: users
+                          .filter(user => user.sponsor === sponsor)
+                          .map(user => user.fans!)
+                          .reduce((a, b) => a + b, 0),
+                      }))}
+                      colors={["#0094df", "#f36b00", "#d31f3c"]}
+                      valueFormatter={v =>
+                        `${v.toLocaleString("en")} total fans`
+                      }
+                      showAnimation
+                      animationDuration={500}
+                    />
+                    <Legend
+                      categories={sponsors}
+                      colors={["#0094df", "#f36b00", "#d31f3c"]}
+                    />
+                  </div>
+
                   <div className="space-y-1">
                     {sponsors.map(sponsor => (
                       <div key={sponsor}>
