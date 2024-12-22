afd551670aeb457964d4c80333d5a93b3f236332
Leon San José Larsson <54620188+leonlarsson@users.noreply.github.com>
2024-12-22 19:50:59 +0100

    fix: Cloudflare?

--- src/components/tables/LeaderboardDataTableColumns.tsx
+++ src/components/tables/LeaderboardDataTableColumns.tsx
@@ -13,7 +13,7 @@ import { LeaderboardId, leaderboards } from "@/utils/leaderboards";
 import SponsorImage from "../SponsorImage";
 import LeagueImage from "../LeagueImage";
 import { useNavigate } from "@tanstack/react-router";
-import { Icons } from "../Icons";
+import Icons from "../Icons";
 
 const columnHelper = createColumnHelper<BaseUserWithExtras>();
 
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
-import { Icons } from "@/components/Icons";
+import Icons from "@/components/Icons";
 import BasicLink from "@/components/Link";
 import { ClubsStatsPanel } from "@/components/panels/ClubsStatsPanel";
 import { LeaderboardStatsPanel } from "@/components/panels/LeaderboardStatsPanel";
